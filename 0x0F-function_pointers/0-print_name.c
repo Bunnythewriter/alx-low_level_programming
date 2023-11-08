93bb2023bbddea8343f9263044ba6a7ef460642c
ambunny <ambanialambo@gmail.com>
2023-11-08 17:48:56 +0300

    Fixed Task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,14 @@
 #include "function_pointers.h"
 
+/**
+* print_name- prints name
+* @name: Name
+* @f: f
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-    if(name == NULL || f == NULL)
-    return;
-    (*f)(name);
+if (name == NULL || f == NULL)
+return;
+(*f)(name);
 }
